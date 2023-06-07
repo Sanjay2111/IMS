e318582290847ca9fd7da36e3235fbf2c374fd7d
sushilkhanal <khanal.sushil47@gmail.com>
2023-06-07 14:55:06 -0500

    store

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,14 @@
-import { createRoot } from "react-dom/client"; // Import from react-dom/client
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/rootReducer";
-import { AlertCountProvider } from "./contexts/AlertCountContext";
-
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { userSlice } from "./features/users/userSlice";
 
-const store = configureStore({
-  reducer: rootReducer, // Pass your rootReducer to the configureStore function
-});
-
-createRoot(document.getElementById("root")).render(
-  <AlertCountProvider>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  </AlertCountProvider>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ApiProvider api={userSlice}>
+      <App />
+    </ApiProvider>
+  </React.StrictMode>
 );
